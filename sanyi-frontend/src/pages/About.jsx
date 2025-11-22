43955182df604e443aa0e97dece674eb962fec81
Yesbol Yerlan <liqiwang5@gmail.com>
2025-11-22 12:03:15 +0100

    feat: added more certificates

--- sanyi-frontend/src/pages/About.jsx
+++ sanyi-frontend/src/pages/About.jsx
@@ -63,7 +63,12 @@ export default function About(){
           scrollbar-hide
         "
       >
-        {['certificate-1.jpg', 'certificate-2.jpg', 'certificate-3.jpg', 'certificate-4.jpg','certificate-5_page-0001.jpg','certificate-5_page-0002.jpg','certificate-5_page-0003.jpg','certificate-5_page-0004.jpg','certificate-6_page-0001.jpg','certificate-6_page-0002.jpg'].map((file, idx) => (
+        {['certificate-1.jpg', 'certificate-2.jpg', 'certificate-3.jpg', 'certificate-4.jpg','certificate-5_page-0001.jpg',
+        'certificate-5_page-0002.jpg','certificate-5_page-0003.jpg','certificate-5_page-0004.jpg','certificate-6_page-0001.jpg',
+        'certificate-6_page-0002.jpg','certificate-7_page_0001.jpg','certificate-7_pages-0002.jpg','certificate-8_page-0001.jpg',
+        'certificate-8_page-0002.jpg','certificate-9_page-0001.jpg','certificate-9_page-0002.jpg','certificate-10_page-0001.jpg',
+        'certificate-10_page-0002.jpg','certificate-11_page-0001.jpg','certificate-11_page-0002.jpg','certificate-12_page-0001.jpg',
+      'certificate-12_page-0002.jpg'].map((file, idx) => (
           <motion.img
             key={idx}
             src={`/assets/licenses/${file}`}
